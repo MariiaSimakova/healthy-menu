a52fa66c6e53119c21debf7be2df9f38c1713157
MariiaSimakova <mariiasimakova6@gmail.com>
2024-05-22 10:48:18 +1000

    send data to server (not json)

--- js/script.js
+++ js/script.js
@@ -224,5 +224,48 @@ window.addEventListener("DOMContentLoaded", () => {
     "menu__item"
   ).render();
 
-  // отправка на сервер
+  // Forms отправка на сервер
+
+  const forms = document.querySelectorAll("form");
+
+  const message = {
+    loading: "Загрузка",
+    success: "Спасибо! Скоро мы с вами свяжемся",
+    fail: "Что-то пошло не так...",
+  };
+
+  forms.forEach((item) => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open("POST", "server.php");
+
+      request.setRequestHeader("Content-type", "multipart/form-data");
+
+      const formData = new FormData(form);
+      request.send(formData);
+      request.addEventListener("load", () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
+        } else {
+          statusMessage.textContent = message.fail;
+        }
+      });
+    });
+  }
 });
